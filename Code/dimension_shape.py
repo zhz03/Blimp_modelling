c5ce64d669d87b966d630047bf4d1bee331c83f4
Zhaoliang <zhz03@g.ucla.edu>
2021-02-19 11:18:32 -0800

    upload the shape dimension code

--- Code/dimension_shape.py
+++ Code/dimension_shape.py
@@ -8,7 +8,7 @@ Created on Mon Feb  8 21:53:25 2021
 
 
 # this is to calcualte the dimension and shape and their relationship
-
+import numpy as np
 global pi 
 pi = 3.14159265
 
@@ -19,6 +19,8 @@ def volume_cal(lift_weight):
     """
     rho_helium = 0.1785 # density of heliumin kg/m^3
     rho_air = 1.225 # density of air in kg/m^3 
+    rho_helium = 0.164 # kg/m^3
+    rho_air = 1.61 # kg/m^3
     volume = lift_weight / (rho_air-rho_helium)
     return volume
 
@@ -53,10 +55,18 @@ def epllipsoid_CSA_cal(x,y):
     """
     CSA = pi * x * y
     return CSA
+
+def triangle_volume_Cal(a,b,c):
+    ratio = pi*(b/2)*(c/2)/(b * c)
+    V = a*b/2 * c * ratio
+    return V    
+
+#def general_model(a,b,c):
     
     
 if __name__=="__main__":
-    
+    """
+    # case 1: white
     test_weight = 0.022 + 0.014 #kg
     a = 0.385/2
     b = 0.40/2
@@ -64,10 +74,55 @@ if __name__=="__main__":
     V = ellipsoid_volume_Cal(a, b, c)
     volume = volume_cal(test_weight) # m^3
     error_V = volume - V
+    error_rate = error_V/volume
     
     CSA_ab = epllipsoid_CSA_cal(a,b)
     
+    # case 2: silver 
+    test_weight_1 = 0.0046 + 0.009 # kg
+    test_weight_1 = 0.0048 + 0.009 # kg
+    a1 = 0.34/2 #30+4.5cm
+    b1 = 0.34/2
+    c1 = 0.24/2
+    c1 = 0.23/2
+    V1 = ellipsoid_volume_Cal(a1, b1, c1)
+    volume1 = volume_cal(test_weight_1) # m^3
+    error_V1 = volume1 - V1
+    error_rate1 = error_V1/volume1
     
+    #case 3: black
+    test_weight_2 = 0.0435 + 0.0335
+    a2 = 0.62/2
+    b2 = 0.635/2
+    c2 = 0.35/2
+    V2 = ellipsoid_volume_Cal(a2, b2, c2)
+    volume2 = volume_cal(test_weight_2) # m^3
+    error_V2 = volume2 - V2    
+    error_rate2 = error_V2/volume2
+    """
+    """
+    #case 4: red
+    test_weight_3 = 0.062 + 0.0337
+    a3 = 0.658/2
+    b3 = 0.658/2
+    c3 = 0.425/2
+    V3 = ellipsoid_volume_Cal(a3, b3, c3)
+    volume3 = volume_cal(test_weight_3) # m^3
+    error_V3 = volume3 - V3    
+    error_rate3 = error_V3/volume3
+    """ 
+    # case 5: triangle
+    test_weight_4 = 0.0576 + 0.0317
+    a4 = 0.709
+    b4 = 0.705
+    c4 = 0.435
     
+    V4_e = ellipsoid_volume_Cal(a4/2, b4/2, c4/2)
+    V4 = triangle_volume_Cal(a4, b4, c4)
+    volume4 = volume_cal(test_weight_4)
+    error_V4 = V4 - volume4
+    error_rate4 = error_V4/volume4
     
-    
+    eq_cir = a4 * b4/2
+    radi = np.sqrt(eq_cir/pi)
+    tick = radi * 2 * 0.599
