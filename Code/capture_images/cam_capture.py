38b85f6cc89e4822cdaa6b52fec51d3bc0323603
Zhaoliang <zhz03@g.ucla.edu>
2021-07-12 18:41:53 -0700

    change different resolution

--- Code/capture_images/cam_capture.py
+++ Code/capture_images/cam_capture.py
@@ -5,14 +5,16 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read()
-        img = cv2.resize(frame,(1280,1080))
+        #img = cv2.resize(frame,(1280,1080))
+        img = cv2.resize(frame, (1024, 768))
         if not ret:
             continue
         k = cv2.waitKey(10)
 
         if k == ord('c'):
             now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
-            cv2.imwrite(f'Images/1280_1080/{now}.jpg', img)
+            #cv2.imwrite(f'Images/1280_1080/{now}.jpg', img)
+            cv2.imwrite(f'Images/1024_768/{now}.jpg', img)
         elif k == ord('q'):
             break
 
