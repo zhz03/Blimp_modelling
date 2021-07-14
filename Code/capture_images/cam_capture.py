5b317ac0a353a4f7a305f47a5b2729cc94e73485
Zhaoliang <zhz03@g.ucla.edu>
2021-07-13 21:57:39 -0700

    change to save different resolution images

--- Code/capture_images/cam_capture.py
+++ Code/capture_images/cam_capture.py
@@ -5,20 +5,24 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read()
-        #img = cv2.resize(frame,(1280,1080))
-        img = cv2.resize(frame, (1024, 768))
+        img1 = cv2.resize(frame,(1280,1080))
+        img2 = cv2.resize(frame, (1024, 768))
+        img3 = cv2.resize(frame, (800, 600))
+        img4 = cv2.resize(frame,(640,480))
         if not ret:
             continue
         k = cv2.waitKey(10)
 
         if k == ord('c'):
             now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
-            #cv2.imwrite(f'Images/1280_1080/{now}.jpg', img)
-            cv2.imwrite(f'Images/1024_768/{now}.jpg', img)
+            cv2.imwrite(f'Images/1280_1080/{now}.jpg', img1)
+            cv2.imwrite(f'Images/1024_768/{now}.jpg', img2)
+            cv2.imwrite(f'Images/800_600/{now}.jpg', img3)
+            cv2.imwrite(f'Images/640_480/{now}.jpg', img4)
         elif k == ord('q'):
             break
 
-        cv2.imshow("image", img)
+        cv2.imshow("image", img1)
 
     # cap.realse()
     cv2.destroyAllWindows()
