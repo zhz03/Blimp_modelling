a032a0f1b137f7b26dfabc1386af10ed737a0e3c
Zhaoliang <zhz03@g.ucla.edu>
2021-07-15 00:19:07 -0700

    upload blynk print info code

---
+++ Code/test_adafruit_esp32_feather_board/Control_02_Blynk_Print_Info/Control_02_Blynk_Print_Info.ino
@@ -0,0 +1,43 @@
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "cLUKk5penym7JGnFxo61dvNzG5p4y4Za";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "lemur";
+char pass[] = "lemur9473";
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
+
+BLYNK_WRITE(V0){
+  int x_0 = param[0].asInt();
+  int y_0 = param[1].asInt();
+  if(x_0=!0 || y_0=!0){
+    Serial.print("%d \t %d ",x_0,y_0);
+    Serial.print("\n"); 
+  }
+  
+}
