8f2f9ea439989b21dcea57ea9ae296a6303c01b5
Zhaoliang <zhz03@g.ucla.edu>
2021-07-14 23:52:58 -0700

    test more than one motor output

--- Code/test_adafruit_esp32_feather_board/test_motor/test_motor.ino
+++ Code/test_adafruit_esp32_feather_board/test_motor/test_motor.ino
@@ -4,7 +4,7 @@
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *myMotor = AFMS.getMotor(3);
-
+Adafruit_DCMotor *myMotor4 = AFMS.getMotor(4);
 void setup() {
   // put your setup code here, to run once:
  
@@ -13,6 +13,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
- myMotor->setSpeed(150); 
+ myMotor->setSpeed(255); // 255 is full
  myMotor->run(FORWARD);
+ myMotor4->setSpeed(255); // 255 is full
+ myMotor4->run(FORWARD);
 }
