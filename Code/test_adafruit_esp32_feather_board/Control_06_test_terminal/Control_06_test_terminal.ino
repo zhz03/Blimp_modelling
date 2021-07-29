6ab0aff6539397a6b099aeccd58939ffbe401b4c
Zhaoliang <zhz03@g.ucla.edu>
2021-07-29 00:35:13 -0700

    test terminal on blynk app

---
+++ Code/test_adafruit_esp32_feather_board/Control_06_test_terminal/Control_06_test_terminal.ino
@@ -0,0 +1,66 @@
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "cLUKk5penym7JGnFxo61dvNzG5p4y4Za";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "lemur";
+char pass[] = "lemur9473";
+
+// Attach virtual serial terminal to Virtual Pin V1
+WidgetTerminal terminal(V3);
+
+// You can send commands from Terminal to your hardware. Just use
+// the same Virtual Pin as your Terminal Widget
+BLYNK_WRITE(V3)
+{
+
+  // if you type "Marco" into Terminal Widget - it will respond: "Polo:"
+  if (String("Marco") == param.asStr()) {
+    terminal.println("You said: 'Marco'") ;
+    terminal.println("I said: 'Polo'") ;
+  } else {
+
+    // Send it back
+    terminal.print("You said:");
+    terminal.write(param.getBuffer(), param.getLength());
+    terminal.println();
+  }
+
+  // Ensure everything is sent
+  terminal.flush();
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Clear the terminal content
+  terminal.clear();
+
+  // This will print Blynk Software version to the Terminal Widget when
+  // your hardware gets connected to Blynk Server
+  terminal.println(F("Blynk v" BLYNK_VERSION ": Device started"));
+  terminal.println(F("-------------"));
+  terminal.println(F("Type 'Marco' and get a reply, or type"));
+  terminal.println(F("anything else and get it printed back."));
+  terminal.flush();
+}
+
+void loop()
+{
+  Blynk.run();
+}
