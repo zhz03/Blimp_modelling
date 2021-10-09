0ac88b408d52a6602bd28f3fe35344376195ada6
Zhaoliang <zhz03@g.ucla.edu>
2021-10-08 22:25:41 -0700

    new test motor code

--- Code/test_adafruit_esp32_feather_board/test_motor/test_motor.ino
+++ Code/test_adafruit_esp32_feather_board/test_motor/test_motor.ino
@@ -2,9 +2,11 @@
 #include <Adafruit_MotorShield.h>
 
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-Adafruit_DCMotor *myMotor = AFMS.getMotor(3);
-Adafruit_DCMotor *myMotor4 = AFMS.getMotor(4);
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *myMotorVertical_L = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotorVetrical_R = AFMS.getMotor(2);
+Adafruit_DCMotor *myMotorLeft = AFMS.getMotor(3);
+Adafruit_DCMotor *myMotorRight = AFMS.getMotor(4);
 void setup() {
   // put your setup code here, to run once:
  
@@ -13,8 +15,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
- myMotor->setSpeed(255); // 255 is full
+  int val = 255;
+ myMotor->setSpeed(val); // 255 is full
  myMotor->run(FORWARD);
- myMotor4->setSpeed(255); // 255 is full
+ delay(5000); // 5 second
+ myMotor4->setSpeed(val); // 255 is full
  myMotor4->run(FORWARD);
 }
